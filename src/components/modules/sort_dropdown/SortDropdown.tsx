c1995d7ab61132a9286cef6fda0be89ea50e2e8f
Raphael <raphael@shellcore.fr>
2023-11-21 20:46:28 +0100

    Refactoring

--- src/components/modules/sort_dropdown/SortDropdown.tsx
+++ src/components/modules/sort_dropdown/SortDropdown.tsx
@@ -1,20 +1,29 @@
+import { useState } from 'react';
 import './SortDropdown.scss';
-import { useEffect, useState } from 'react';
 
 const SortDropdown = () => {
   const [state, setState] = useState(false);
 
-  const handleFocus = (e: any) => {
+  const handleClickDropdown = () => {
     setState(prevState => !prevState);
   };
 
   return <div className='SortDropdown'>
     <div>
-      <a href='#' tabIndex={3} onFocus={handleFocus} >Popularité</a>
+      <div>
+        <button tabIndex={3} onClick={handleClickDropdown}>
+          Popularité
+          <div className={`${state ? 'rotate' : 'unrotate'}`}>
+            <svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M14.12 0.453125L8 6.55979L1.88 0.453125L0 2.33312L8 10.3331L16 2.33312L14.12 0.453125Z" fill="white"/>
+            </svg>
+          </div>
+        </button>
+      </div>
       {state &&
         <div className={'SortDropdownContent'}>
           <a href='#' tabIndex={4}>Date</a>
-          <a href='#' tabIndex={5}>Titre</a>
+          <a href='#' tabIndex={5} onBlur={handleClickDropdown}>Titre</a>
         </div>
       }
     </div>
